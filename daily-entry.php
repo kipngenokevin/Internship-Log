12c536cbaf8d6039d3114f26eb18660066ca2f88
Kevin Kipngeno <35192411+kipngenokevin@users.noreply.github.com>
2021-05-19 12:06:55 +0300

    First Commit

---
+++ daily-entry.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daily Entry</title>
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="css/daily-entry.css" rel="stylesheet" type="text/css"> 
+    <link rel="stylesheet" href="css/style.css" rel="stylesheet" type="text/css"> 
+</head>
+<body>
+<html lang="en">
+<head>
+  <title>Internship Program</title>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<input type="checkbox" id="nav-toggle">
+  <div class="sidebar">
+    <div class="sidebar-brand">
+      <h1><span><img  src="img/jkuat-logo.png" width="50px" height="50px" alt=""></span><span>Internship</span></h1>
+    </div>
+    <div class="sidebar-menu">
+      <ul>
+        <li><a href="index"><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+        <li><a href="daily-entry" class="active"><span class="las la-pen-nib"></span>
+          <span>New Daily Entry</span></a>
+        </li>
+        <li><a href=""><span class="las la-list"></span>
+          <span>View Daily Entries</span></a>
+        </li>
+        <li><a href="weekly-report"><span class="lar la-edit"></span>
+          <span>Write Weekly Report</span></a>
+        </li>
+        <li><a href=""><span class="las la-clipboard-list"></span>
+          <span>View Weekly Reports</span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+
+  <div class="main-content">
+
+    <header>
+      <h2>
+        <label for="nav-toggle">
+          <span class="las la-bars"></span>
+        </label>
+        Dashboard
+      </h2>
+      <div class="search-wrapper">
+        <span class="las la-search"></span>
+        <input type="search" placeholder="Search here">
+      </div>
+      <div class="user-wrapper">
+        <img src="img/img.png" width="50px" height="50px" alt="">
+        <div>
+          <h4>John Doe</h4>
+          <small>Student Account</small>
+        </div>
+      </div>
+    </header>
+
+
+  
+         <section>
+            <div class="container">
+                <div class="contactInfo">
+                  <div>
+                    <h2> Daily Entry </h2>
+                    <h2><span class="las la-edit"></span>19/05/2021</h2>
+                  </div>
+                </div>
+                <div class="contactForm">
+                  <h2>Write Today's Entry</h2>
+                  <div class="formBox">
+                    <div class="inputBox w50">
+                      <input type="text" required>
+                      
+                    </div>
+                  </div>
+                </div>
+            </div>
+         </section>
+
+  </div>
+
+</body>
+</html>
